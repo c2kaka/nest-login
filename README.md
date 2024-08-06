a76f73245cf2814e654d773e1fc74e392dbaecaf
Ouyangfan <fan.ouyang@transwarp.io>
2024-08-06 12:24:46 +0800

    feat: use nestjs/jwt to generate token

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
-## Install express-session
+## Session
+### Install express-session
 ```bash
 npm install express-session @types/express-session
+```
+
+## JWT
+### Install @nestjs/jwt
+```bash
+npm install @nestjs/jwt
 ```
--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,8 +1,21 @@
-import { Controller, Get, Session } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Headers,
+  Inject,
+  Res,
+  Session,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { AppService } from './app.service';
+import { JwtService } from '@nestjs/jwt';
+import { Response } from 'express';
 
 @Controller()
 export class AppController {
+  @Inject(JwtService)
+  private readonly jwtService: JwtService;
+
   constructor(private readonly appService: AppService) {}
 
   @Get()
@@ -14,5 +27,28 @@ export class AppController {
   getSession(@Session() session): string {
     session.views = session.views ? session.views + 1 : 1;
     return `Views: ${session.views}`;
+  }
+
+  @Get('jwt')
+  getJwt(
+    @Headers('authorization') authorization: string,
+    @Res({ passthrough: true }) response: Response,
+  ): number {
+    if (authorization) {
+      try {
+        const token = authorization.split(' ')[1];
+        const data = this.jwtService.verify(token);
+        const newToken = this.jwtService.sign({ count: data.count + 1 });
+        response.setHeader('token', newToken);
+        return data.count + 1;
+      } catch (e) {
+        console.log(e);
+        throw new UnauthorizedException();
+      }
+    } else {
+      const newToken = this.jwtService.sign({ count: 1 });
+      response.setHeader('token', newToken);
+      return 1;
+    }
   }
 }
